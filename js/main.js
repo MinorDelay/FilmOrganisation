cf8253a960dbdb7a7ec5a181427ad89f0f55d545
maylinnr <47450041+maylinnr@users.noreply.github.com>
2019-10-08 18:37:00 +0200

    In progress to save selection

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 
+function setValue(selection, selectedProperty) {
+    let tagValue = document.tagSelection.tag.value; // Get selected tag to change
+    let listValue = selection.value;
+    let tagList = document.getElementsByTagName(tagValue); // Get list of selected tag
+    for (let i = 0; i < tagList.length; i++) {
+        tagList[i].style[selectedProperty] = listValue; // Change input property
+    }
 
+    // Save presets in sessionStorage
+    sessionStorage.setItem(tagValue + "_" + selectedProperty, listValue);
+}
