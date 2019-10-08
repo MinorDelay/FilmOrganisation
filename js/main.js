7a82b34065b0963ebfb30b9b9d5b31cd6cc0b653
maylinnr <47450041+maylinnr@users.noreply.github.com>
2019-10-08 19:56:29 +0200

    Save selected value

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 
-function setValue(selection, selectedProperty) {
-    let tagValue = document.tagSelection.tag.value; // Get selected tag to change
-    let listValue = selection.value;
-    let tagList = document.getElementsByTagName(tagValue); // Get list of selected tag
-    for (let i = 0; i < tagList.length; i++) {
-        tagList[i].style[selectedProperty] = listValue; // Change input property
-    }
+var selectedMood;
+var selectedRating;
 
-    // Save presets in sessionStorage
-    sessionStorage.setItem(tagValue + "_" + selectedProperty, listValue);
+function setMood(selection) {
+    selectedMood = selection.value;
+}
+
+function setRating(selection) {
+    selectedRating = selection.value;
 }
