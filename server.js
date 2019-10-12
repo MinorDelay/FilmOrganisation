a75109558b29f8c39077f844ad308e26df6ec67c
maylinnr <47450041+maylinnr@users.noreply.github.com>
2019-10-12 15:11:42 +0200

    Creating database

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 var express = require('express')
 var app = express();
+var sqlite3 = require('sqlite3')
+
+// routes
+app.get('/public', function(request, response){
+    response.send('Hello, World');
+});
+
+// Webpage with a table with all movies
+app.get('public/movieslist', function(request, response){
+    console.log("GET request received at /movielist");
+});
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
