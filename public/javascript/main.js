2b540f76e0658a0e0a905adc4c68ad51fa497642
maylinnr <47450041+maylinnr@users.noreply.github.com>
2019-10-13 12:30:08 +0200

    Database connected

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,4 +1,3 @@
-
 var selectedMood;
 var selectedRating;
 
@@ -9,3 +8,35 @@ function setMood(selection) {
 function setRating(selection) {
     selectedRating = selection.value;
 }
+
+// Handle database
+function showMovies(movies) {
+    var moviesList = document.getElementById('moviesList');
+    for (var i = 0; i < movies.length; i++) {
+        var movie = document.createElement('movie');
+        movie.className += 'movieList';
+        var heading = document.createElement('h3');
+        heading.innerHTML = movies[i].title;
+        var summary = document.createElement('p');
+        summary.innerHTML = movies[i].summary;
+        movie.appendChild(heading);
+        movie.appendChild(summary);
+        moviesList.appendChild(movie)
+    }
+}
+
+function getMovies() {
+    $.get('/allmovies', function (data) {
+        if (!data) {
+            console.log("No data received")
+        } else {
+            console.log("Data received");
+            for (var i = 0; i < data.length; 1++) {
+                console.log(data[i].title);
+            }
+        }
+        showMovies(data)
+    });
+}
+
+getMovies();
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 var express = require('express')
 var app = express();
-var sqlite3 = require('sqlite3')
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('data/movies.sqlite')
+
+app.use(express.static(__dirname + '/public'))
 
 // routes
-app.get('/public', function(request, response){
+app.get('/', function(request, response){
     response.send('Hello, World');
 });
 
 // Webpage with a table with all movies
-app.get('public/movieslist', function(request, response){
+app.get('/allmovies', function(request, response){
     console.log("GET request received at /movielist");
+    db.all('SELECT * FROM movies', function(err, rows){
+        if(err){
+            console.log("Error: " + err)
+        }
+        else{
+            response.send(rows);
+        }
+    })
 });
 
 app.listen(3000, function(){
