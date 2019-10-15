84f986d1e5b877d80e76da6b8c0498437375a067
MinorDelay <36192902+MinorDelay@users.noreply.github.com>
2019-10-15 14:21:26 +0200

    update

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.get('/allmovies', function (request, response) {
     })
 });
 
+//////////////////////////// Search movies from home page ////////////////////////////
 var userFilter;
 
 // Process filter chosen by user
@@ -44,24 +45,26 @@ app.get('/searchResult', function (request, response) {
     if (userFilter.title != '') sqlQuery += " AND title LIKE '%" + userFilter.title + "%'";
 
     if (userFilter.imdbRating == "< 5") sqlQuery += " AND imdbRating <= 5";
-    else if (userFilter.imdbRating == "5 - 7") sqlQuery += " AND imdbRating >= 5 AND imdbRating <= 7";
+    else if (userFilter.imdbRating == "5 - 7.5") sqlQuery += " AND imdbRating BETWEEN 5 AND 7";
     else if (userFilter.imdbRating == "> 7.5") sqlQuery += " AND imdbRating >= 7.5";
 
     if (userFilter.price == "Cheap watch (< 5)") sqlQuery += " AND price <= 5";
-    else if (userFilter.price == "Budget (5 - 10)") sqlQuery += " AND price >= 5 AND price <= 10";
-    else if (userFilter.price == "Medium (10.01 - 15)") sqlQuery += " AND price > 10.00 AND price <= 15";
-    else if (userFilter.price == "Expensive (> 15)") sqlQuery += " AND price >= 15 ";
+    else if (userFilter.price == "Budget (5 - 10)") sqlQuery += " AND price BETWEEN 5 AND 10";
+    else if (userFilter.price == "Medium (10 - 15)") sqlQuery += " AND price BETWEEN 10.00 AND 15";
+    else if (userFilter.price == "Expensive taste (> 15)") sqlQuery += " AND price >= 15 ";
 
     if (userFilter.mood != '') sqlQuery += " AND mood = '" + userFilter.mood + "'";
 
     if (userFilter.genre != '') sqlQuery += " AND genre = '" + userFilter.genre + "'";
 
-    if (userFilter.length == "< 90 minutes") sqlQuery += " AND length <= 90";
-    else if (userFilter.length == "90 - 120 minutes") sqlQuery += " AND price >= 90 AND price <= 120";
-    else if (userFilter.length == "> 120 minutes") sqlQuery += " AND price >= 120";
+    if (userFilter.length == "< 30 minutes") sqlQuery += " AND length <= 30";
+    else if (userFilter.length == "30 - 90 minutes") sqlQuery += " AND length BETWEEN 30 AND 90";
+    else if (userFilter.length == "> 90 minutes") sqlQuery += " AND length >= 90";
+
+    console.log(sqlQuery)
 
     // Return query result
-    db.all(sqlQuery.toString(), function (err, rows) {
+    db.all(sqlQuery, function (err, rows) {
         if (err) {
             console.log("Error: " + err)
         } else {
@@ -70,6 +73,18 @@ app.get('/searchResult', function (request, response) {
     })
 });
 
+//////////////////////////// Search movies from by indirect search ////////////////////////////
+var characteristic;
+
+app.post('/pageLinking', function (request, response) {
+    console.log("POST request received at /search");
+    console.log(request.body)
+    userFilter = request.body;
+    response.redirect('/searchresult.html');
+});
+
+
+//////////////////////////// General functionality ////////////////////////////
 app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });
